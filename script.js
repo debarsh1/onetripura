8aab8ebdb77ea7e8e160b1ce347c86bf90bd7ea8
Debarshi Chakma <73898025+debarsh1@users.noreply.github.com>
2024-10-30 23:40:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-function showContactForm() {
-    document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
-}
 
-document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Thank you for reaching out to us! We will get back to you shortly.");
-    this.reset();
-});
